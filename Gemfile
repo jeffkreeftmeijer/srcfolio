4d5b23cfc2f1bdc4079ab66ca354b6c5615496b5
jeffkreeftmeijer <jeff@kreeftmeijer.nl>
2009-12-15 01:07:38 +0100

    switched to mongodb

--- Gemfile
+++ Gemfile
@@ -4,6 +4,10 @@ source "http://gemcutter.org"
  
 gem "rails", "2.3.5"
 
+gem "mongo",        "0.18.0"
+gem "mongo_ext",    "0.18.0"
+gem "mongo_mapper", "0.6.7"
+
 only :test do
   gem "cucumber", "0.4.4"
 end
--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,6 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.frameworks -= [:active_record]
   config.time_zone = 'UTC'
 end
