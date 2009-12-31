feba3b33b2b8e6a5ca861a4b1280275221aa4010
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2009-12-31 16:39:58 +0100

    subdomains! :D

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
-  map.contributor_with_subdomain '', :controller => 'contributors', :action => 'show', :conditions => { :subdomain => /.+/ }
+  map.contributor_with_subdomain '', :controller => 'contributors', :action => 'show',  :conditions => { :subdomain => /.+/ }
   
-  map.resources :contributors, :only => [:index, :show]
+  map.resources :contributors, :only => [:index, :show], :conditions => { :subdomain => false }
   map.root      :controller => :contributors
   
-  map.namespace :admin do |admin|
+  map.namespace :admin, :conditions => { :subdomain => false } do |admin|
     admin.root :controller => :jobs
     admin.resources :contributors, :only => :index
     admin.resources :jobs, :only => :index
