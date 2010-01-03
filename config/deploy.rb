63535205c5a3081726bd85e426fa21f7343e6028
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2010-01-03 21:30:45 +0100

    added the "Move in database.yml" task

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,6 +16,11 @@ namespace :deploy do
   task :restart, :roles => :app do
     run "touch #{current_path}/tmp/restart.txt"
   end
+  
+  desc "Move in database.yml for this environment"
+  task :move_in_database_yml, :roles => :app do
+    run "cp #{deploy_to}/shared/database.yml #{current_path}/config/"
+  end
  
   desc "Run gem bundle"
   task :bundle, :roles => :app do
@@ -24,3 +29,4 @@ namespace :deploy do
 end
 
 after "deploy:update_code", "deploy:bundle"
+after "deploy:symlink", "deploy:move_in_database_yml"
