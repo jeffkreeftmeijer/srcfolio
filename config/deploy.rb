91e0788cbfce11eebb1d8b9737879de83b930aac
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2010-01-03 20:27:31 +0100

    added bundler and restart tasks in the deploy recipe

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,8 +6,21 @@ role :db,  "srcfolio.com", :primary => true
 
 set :scm, :git
 set :repository,  "git://github.com/jeffkreeftmeijer/srcfolio.git"
-set :deploy_to,   "~/app"
 set :use_sudo,    false
  
 set :group, "srcfolio"
 set :user,  "srcfolio"
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+ 
+  desc "Run gem bundle"
+  task :bundle, :roles => :app do
+    run "cd #{release_path} && gem bundle --cached"
+  end
+end
+
+after "deploy:update_code", "deploy:bundle"
